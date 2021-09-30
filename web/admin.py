c32a6a4e3f543c744b1b89d74a23c723954b0f88
farshad17ph <farshad.farnaghi@gmail.com>
2021-09-30 10:39:45 +0330

    income in models and beautification

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Expence
+from .models import Expence, Income
+
 
 admin.site.register(Expence)
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 class Expence(models.Model):
     text = models.CharField(max_length = 255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format( self.date, self.amount )
+
+class Income(models.Model):
+    text = models.CharField(max_length = 255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format( self.date, self.amount )
